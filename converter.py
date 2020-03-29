22159c15efd6ca224d835bc85514373749b213b5
Fei Niu <fniu@apple.com>
2020-03-29 12:16:31 -0700

    Word normailization

--- converter.py
+++ converter.py
@@ -35,40 +35,40 @@ def read_db(kindle_db_path):
     return word_and_context_pairs
 
 
+# try shifting the tense to present or falling back to original form
+def normalize_word(wordnet_lemmatizer, word):
+    normalized_word = wordnet_lemmatizer.lemmatize(word, 'v')
+    if normalized_word and normalized_word != word:
+        return normalized_word
+
+    normalized_word = wordnet_lemmatizer.lemmatize(word, 'n')
+    if normalized_word and normalized_word != word:
+        return normalized_word
+
+    return word
+
+
 def to_eudic_csv(word_and_context_pairs, date_suffix):
-    eudic_file_path = r"/Users/fniu/kindle/kindleconverter/csvs/" \
-                      r"vocab_{}_with_tense.csv" \
+    eudic_file_path = r"/Users/fniu/kindle/kindleconverter/csvs/vocab_{}.csv" \
         .format(date_suffix)
+    wordnet_lemmatizer = WordNetLemmatizer()
     with open(eudic_file_path, 'w') as out_file:
         for word, context in word_and_context_pairs.items():
+            trimmed_word = word.strip().lower()
+            normalized_word = normalize_word(wordnet_lemmatizer, trimmed_word)
+
             trimmed_context = context \
                 .replace("’", "'") \
                 .replace(" ", " ") \
                 .replace("—", " ") \
-                .replace("“", "\"") \
-                .replace("”", "\"") \
+                .replace("“", "'") \
+                .replace("”", "'") \
+                .replace("\"", "'") \
                 .replace("\n", " ")
-            out_file.write(word + "," + trimmed_context + "\n")
+
+            out_file.write(normalized_word + "," + trimmed_context + "\n")
     print("Wrote csv file at {}".format(eudic_file_path))
     return eudic_file_path
-
-
-def remove_tense(eudic_file_path, date_suffix):
-    csv_without_tense = r"/Users/fniu/kindle/kindleconverter/csvs/" \
-                        r"vocab_{}.csv" \
-        .format(date_suffix)
-    wordnet_lemmatizer = WordNetLemmatizer()
-    with open(csv_without_tense, 'w') as out_file:
-        with open(eudic_file_path, 'r') as in_file:
-            for line in in_file.readlines():
-                first_comma_index = line.index(",")
-                word = line[0:first_comma_index]
-                context = line[first_comma_index:-1]
-                word_to_write = wordnet_lemmatizer.lemmatize(word, 'v')
-                if not word_to_write:
-                    word_to_write = word
-                out_file.write(word_to_write + "," + context + "\n")
-    print("Written  csv without tense at {} .".format(csv_without_tense))
 
 
 def main():
@@ -78,9 +78,12 @@ def main():
     print("date_suffix={}".format(date_suffix))
 
     backup_db_path = copy_db(kindle_db_path, date_suffix)
+    # date_suffix = r"2020-03-28_11-35-38"
+    # backup_db_path = r"/Users/fniu/kindle/kindleconverter/dbs/vocab_{}.db"\
+    #     .format(date_suffix)
+
     word_and_context_pairs = read_db(backup_db_path)
-    eudic_file_path = to_eudic_csv(word_and_context_pairs, date_suffix)
-    remove_tense(eudic_file_path, date_suffix)
+    to_eudic_csv(word_and_context_pairs, date_suffix)
 
 
 if __name__ == "__main__":
